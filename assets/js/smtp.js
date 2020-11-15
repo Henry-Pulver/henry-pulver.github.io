0cdee3bf0f538cccd56985e99ea22fd680a1e97d
Henry Pulver <henry.pulver@five.ai>
2020-11-15 19:04:42 +0000

    Add possibility of sending emails from contact form

---
+++ assets/js/smtp.js
@@ -0,0 +1,17 @@
+﻿/* SmtpJS.com - v3.0.0 */
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+
+function send_form_email(name, email, field, occupation, qualification, institution) {
+
+    var email_body = ""
+    Email.send({
+        SecureToken: "a44948a8-9f25-42a8-ab50-277628fbb857",
+        To: 'extended@extend.education',
+        From: "smtp@smtp.mail",
+        Subject: "Another sign-up!",
+        Body: email_body
+    }).then(
+        message => alert(message)
+    );
+}
